3118e3b7b1d44597fd4b7022789d55b0ec98e795
njayman <najishm828282@gmail.com>
2025-06-16 15:19:37 +0600

    added arlopeeker in projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,16 +3,25 @@ import Link from "next/link";
 
 export default function Projects() {
   const githubProfileBaseUrl = "https://githib.com/njayman";
+
   return (
     <Article>
       <h1 className="text-4xl">Awesome projects</h1>
-      <section>
+      <section className="pt-10">
         <h2 className="text-2xl">
           <Link href={`${githubProfileBaseUrl}/proz`} target="_blank">
             Proz
           </Link>
         </h2>
         <p>A terminal based project listing tool.</p>
+      </section>
+      <section className="pt-10">
+        <h2 className="text-2xl">
+          <Link href={`${githubProfileBaseUrl}/arlopeeker`} target="_blank">
+            Arlopeeker
+          </Link>
+        </h2>
+        <p>A desktop status app that acts as a peeker.</p>
       </section>
       <span>more to come</span>
     </Article>
